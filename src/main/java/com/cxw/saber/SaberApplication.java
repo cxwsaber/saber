79b1bf5024e24c3ce034e186489d6504c2c22a39
linuxsaber <1032981913@qq.com>
2021-11-24 21:51:40 +0800

    测试机4

--- src/main/java/com/cxw/saber/SaberApplication.java
+++ src/main/java/com/cxw/saber/SaberApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SaberApplication {
     //添加测试
-//tianji
     //添加测试3
     public static void main(String[] args) {
         SpringApplication.run(SaberApplication.class, args);
