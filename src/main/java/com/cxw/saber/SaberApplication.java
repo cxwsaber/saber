ab0875ff4f355ef9ec9572b2a70971f52cc3de05
linuxsaber <1032981913@qq.com>
2021-11-25 09:40:19 +0800

    测试纠错7

--- src/main/java/com/cxw/saber/SaberApplication.java
+++ src/main/java/com/cxw/saber/SaberApplication.java
@@ -9,6 +9,7 @@ public class SaberApplication {
     //添加测试3
     //测试纠错
     //测试纠错5
+    //测试纠错七
     public static void main(String[] args) {
         SpringApplication.run(SaberApplication.class, args);
     }
